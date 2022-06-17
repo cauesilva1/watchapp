49e4e88be61f067fe2b2156a0de74a4375b2520d
caue <cauecatonesilva@gmail.com>
2022-06-17 17:46:32 -0400

    cronometro

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-
 let tempo = 1000;
 let cronometro;
-let hh = 0
-let min = 0
-let sec = 0
+let hh = 0;
+let min = 0;
+let sec = 0;
+let button = 0;
+
 
 function start(){
-    cronometro =  setInterval(() => { timer();
-    }, tempo);
-
-    if(start = 1){
-        start.disabled = true;
+    button++;
+    if(button<2){
+    cronometro =  setInterval(() => { timer();}, tempo);
     }
+
+
 }
 
 function stop(){
+    button = 0;
     clearInterval(cronometro);
     hh = hh;
     min = min;
@@ -23,30 +25,35 @@ function stop(){
 }
 
 function reset(){
+    button=0;
     clearInterval(cronometro);
 
-     hh = 0
-     min = 0
-     sec = 0
+    hh = 0;
+    min = 0;
+    sec = 0;
 
-     document.getElementById('hr').innerText = hh
-     document.getElementById('min').innerText = min
-     document.getElementById('sec').innerText = sec
+    document.getElementById('hr').innerText = hh
+    document.getElementById('min').innerText = min
+    document.getElementById('sec').innerText = sec
 }
 
 function timer(){
     sec++;
 
     if(sec == 60){
-        sec = 00;
+        sec = 0;
         min++;
 
         if(min==60){
-            min =00;
+            min =0;
             hr++;
         }
+
     }
     document.getElementById('hr').innerText = hh
     document.getElementById('min').innerText = min
     document.getElementById('sec').innerText = sec
+
+
+
 }
